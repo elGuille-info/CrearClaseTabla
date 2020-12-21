d5dcb601ce2fa07dc98d3678899139ccece4b781
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2020-12-21 13:02:42 +0100

    Initial commit

---
+++ CrearClaseTabla_VB/My Project/AssemblyInfo.vb
@@ -0,0 +1,36 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("gsCrearClaseTabla_VB")>
+<Assembly: AssemblyDescription("Generar clases a partir de una tabla de una base de datos de SQL Server o de Access  (revisión del 21-dic-2020)")>
+<Assembly: AssemblyCompany("elGuille")>
+<Assembly: AssemblyProduct("gsCrearClaseTabla_VB")>
+<Assembly: AssemblyCopyright("©Guillermo Som (elGuille), 2004-2007, 2018-2020")>
+<Assembly: AssemblyTrademark("Microsoft Visual Basic 2019 (.NET 4.8)")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("62b1f227-5904-448f-8ede-dd2a36a376fa")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("2.0.0.0")>
+<Assembly: AssemblyFileVersion("2.0.0.15")>
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# CrearClaseTabla
+ Generar clases a partir de una tabla de una base de datos de SQL Server o de Access
